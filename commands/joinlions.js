adb2ed1cf9e6c35fb4407a4cb2740b21a85dc3b8
Ahsany <39676871+Ahsany@users.noreply.github.com>
2018-08-04 14:50:05 +0100

    Update joinlions.js

--- commands/joinlions.js
+++ commands/joinlions.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const weather = require('weather-js')
-const fs = require('fs')
 const prefix = '-'
 const owner = '321673115891531787'
 
