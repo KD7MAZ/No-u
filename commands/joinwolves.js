dc80bf70c4b7da2fe6a66628eb86b1fbd8cb2f7f
Ahsany <39676871+Ahsany@users.noreply.github.com>
2018-08-04 14:35:06 +0100

    Create joinwolves.js

---
+++ commands/joinwolves.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const weather = require('weather-js')
+const prefix = '.'
+const owner = '321673115891531787'
+
+exports.run = (bot, message, args) => {
+
+if (message.guild.id !== '323206382147076096') return  
+
+const bears = message.guild.roles.find("name", "Faction: Bears");
+const wolves = message.guild.roles.find("name", "Faction: Wolves");
+const lions = message.guild.roles.find("name", "Faction: Lions");
+
+    if(message.member.roles.find("name", "Faction: Bears")) {
+        return message.channel.send(`:x: ${message.author} **You can't join the faction __Wolves__, as you are in the faction __Bears__, if this was a mistake please pm a moderator with proof of your paint!**`) 
+        
+    }
+    if(message.member.roles.find("name", "Faction: Lions")) {        
+        return message.channel.send(`:x: ${message.author} **You can't join the faction __Wolves__, as you are in the faction __Bears__, if this was a mistake please pm a moderator with proof of your paint!**`)        
+    
+    }
+    if(message.member.roles.find("name", "Faction: Wolves")) {        
+        return message.channel.send(`:x: ${message.author} **You are already in the faction __Wolves__!**`)        
+    
+    }
+
+    message.member.addRole(wolves)
+    message.channel.send(`:white_check_mark: ${message.author} **You Successfully joined the faction __Wolves__, you now have access to <#475280783033237504>!**`) 
+        
+
+}
+
