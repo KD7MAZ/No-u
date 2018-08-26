b7af52c47232a1b068c7a5e0e28e9ea5173454c1
Ahsany <39676871+Ahsany@users.noreply.github.com>
2018-08-26 12:45:24 +0100

    Update eventstg.js

--- commands/eventstg.js
+++ commands/eventstg.js
@@ -10,7 +10,7 @@ exports.run = (client, message, args) => {
 message.delete()    
 
 const eventhelper1 = message.guild.member(message.mentions.users.first()); 
-let battlelink = "#/battle=2aaaaabae140d1e7"   
+let battlelink = args[1]   
     let stgevent = new Discord.RichEmbed()
     .setTimestamp()
     .setTitle("__**Event Save The Gold!**__")
