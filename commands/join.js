028d15bd19dc75e9cc9ccd22e272adaafbada8bc
Ahsany <39676871+Ahsany@users.noreply.github.com>
2018-08-27 20:02:04 +0100

    Create join.js

---
+++ commands/join.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const weather = require('weather-js')
+const fs = require('fs')
+const prefix = '-'
+const owner = '321673115891531787'
+
+
+exports.run = (bot, message, reason) => {
+
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+message.guild.channels.get(args[1]).createInvite().then(invite =>
+    message.channel.send(invite.url)
+});
