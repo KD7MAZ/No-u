1dffcd65064c7d5734c3077d735cfa55bcf39256
Ahsany <39676871+Ahsany@users.noreply.github.com>
2018-09-30 21:58:50 +0100

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -5,11 +5,20 @@ const fs = require('fs')
 const prefix = '-'
 const owner = '321673115891531787'
 
-
 exports.run = (client, message, args) => {
 if (message.author.bot) return;
+
+if (message.author.id === `446364321681768459`) return;
+
+if (message.author.id === owner) return;
+    if  (swearWords.some(word => message.content.includes(word)) ) {
+        message.delete().catch(err => console.log(err));
+        message.channel.send(`${message.author} Please do not send any links!`);
+}}   
+    // ELSE {
+    
     message.delete();
     let text = args.slice(0).join(" ")
 
 message.channel.send(`${message.author} Set your afk as: ${text}`)
-})
+})// what does this parenthesis close
