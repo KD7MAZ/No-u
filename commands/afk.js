f0e27f3b757c870d0046fd342e9a78b04fe119a2
Ahsany <39676871+Ahsany@users.noreply.github.com>
2018-09-30 21:10:50 +0100

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -8,8 +8,17 @@ const owner = '321673115891531787'
 
 exports.run = (client, message, args) => {
 if (message.author.bot) return;
+ const swearWords = ["http", "https", "Http", "Https", "HTTP", "HTTPS", "HTtp", "HTtp", "HTTps", "HTTPs", "hTtp", "hTtps", "hTTp", "hTTps", "hTTP", "hTPPs", "hTTPS", "HtTp", "HtTps", "HtTP", "HtTPS"];
+if (message.author.id === `446364321681768459`) return;
+if (message.author.id === owner) return;
+    if  (swearWords.some(word => message.content.includes(word)) ) {
+        message.delete().catch(err => console.log(err));
+        message.channel.send(`${message.author} Please do not send any links!`);
+}}   
+    
+    
     message.delete();
     let text = args.slice(0).join(" ")
 
 message.channel.send(`${message.author} Set your afk as: ${text}`)
-}
+})
