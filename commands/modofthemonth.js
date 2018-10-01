8e78273c2d85ab6c3156dfa948ae7a77847a6039
Ahsany <39676871+Ahsany@users.noreply.github.com>
2018-10-01 21:03:28 +0100

    Update modofthemonth.js

--- commands/modofthemonth.js
+++ commands/modofthemonth.js
@@ -5,17 +5,17 @@ const fs = require('fs')
 const prefix = '-'
 const owner = '321673115891531787'
 
-exports.run = (client, message, args) => {
+exports.run = (client, message, reason) => {
     if (message.author.id !== `${owner}`) return message.channel.send(`**|:exclamation:|  You dont have permission to use this command**`)
     message.delete()    
 const nmofm = message.guild.member(message.mentions.users.first()); 
 
     let motm = new Discord.RichEmbed()
     .setTimestamp()
-    .setTitle("__**Mod Of The Month Reset!**__ | Month: July")
+    .setTitle("__**Mod Of The Month Reset!**__ | Month: September")
     .setColor("#8F00FF")
-    .setDescription('The hot month of july is over as as alot of you will go to hot places on holiday!')
-    .addField("New Mod Of The Month:", `${nmofm.user.username}#${nmofm.user.discriminator} (${nmofm.user}) and is given the role: (<@&397741318979190784>) \n \nI have given to it my gay clan member black because he is fun to play with in tnaki and hes also very funny in discord and does help me alot with things i need! \nCongratulations on this! Keep up the great work.`)
+    .setDescription('The cold months of autum in full swing!')
+    .addField("New Mod Of The Month:", `${nmofm.user.username}#${nmofm.user.discriminator} (${nmofm.user}) and is given the role: (<@&481037960603500544>) \n \nI gave it to manos because he is a really good and hard working moderator, he is active and he enforces punishments on people even though they are his friend thats what i like most about him! Cg keep up with good work :tada:`)
     message.channel.send(motm);
 }
 
