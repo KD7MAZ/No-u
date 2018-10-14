ddd3eda8d09b22375bd27584b05e809fc6fb12b6
Ahsany <39676871+Ahsany@users.noreply.github.com>
2018-10-14 14:23:42 +0100

    hm.......

--- commands/ban.js
+++ commands/ban.js
@@ -8,6 +8,11 @@ const owner = '321673115891531787'
 
 exports.run = (bot, message, [mention, ...reason]) => {
     const banMember = message.mentions.members.first();
+    
+    if (!message.member.hasPermission("BAN_MEMBERS")){
+      return message.channel.send(`:x: ${message.author} You do not have ban members permission`);
+    }
+
     if (message.mentions.members.size === 0){
       let Incorrect = new Discord.RichEmbed()
       .setTimestamp()
@@ -24,15 +29,7 @@ exports.run = (bot, message, [mention, ...reason]) => {
     .setDescription(`**Description:** Bans a member from the server \n**Usage:** ` +prefix + `ban <user> ➡[reason]⬅ \n**Examples:** \n` +prefix + `ban @Ahsan No u \n` +prefix + `ban @Ahsan Why are u a noob \n` +prefix + `ban @Ahsan Spamming too much \n` +prefix + `ban @Ahsan Advertising via dms\n**Error:** Did not specify a reason`)
     return message.channel.send(Incorrect);}
   
-    if (!message.member.hasPermission("BAN_MEMBERS"))
-      return message.channel.send(`:x: ${message.author} You do not have ban members permission`);
     
-    if (message.author.id == message.mentions.users.first()) 
-      return message.channel.send(`:x: ${message.author} You cannot do that to yourself, why did you try?`);
-    
-    
-
-      message.delete()
       const ahsanbanMsgemBed = new Discord.RichEmbed()
       .setTimestamp()
       .setTitle("__**Banned!**__")
@@ -41,10 +38,19 @@ exports.run = (bot, message, [mention, ...reason]) => {
       .addField("User Banned:", `Ahsan#3247 (<@321673115891531787>)`)
       .addField("Banned By:", `${message.author.username}#${message.author.discriminator} (${message.author})`)
       .addField("Reason:", reason.join(' '));
+      
       if (message.content.startsWith(prefix + 'ban <@321673115891531787>')){
-  return message.channel.send(ahsanbanMsgemBed + `<a:WAVE:437630980480827403> **Ahsan#3247 has just left ${message.guild.name} :CRY: the server now has ${message.guild.memberCount} Members!** \nI guess Ahsan#3247 didnt want to follow the rules!`)}
-else if (message.content.startsWith(prefix + 'ban <@!321673115891531787>')){
-  return message.channel.send(ahsanbanMsgemBed + `<a:WAVE:437630980480827403> **Ahsan#3247 has just left ${message.guild.name} :CRY: the server now has ${message.guild.memberCount} Members!** \nI guess Ahsan#3247 didnt want to follow the rules!`)}
+   message.channel.send(`<a:WAVE:437630980480827403> **Ahsan#3247 has just left ${message.guild.name} :CRY: the server now has ${message.guild.memberCount} Members!** \nI guess Ahsan#3247 didnt want to follow the rules!`)
+  return message.channel.send(ahsanbanMsgemBed )}
+
+  else if (message.content.startsWith(prefix + 'ban <@!321673115891531787>')){
+    message.channel.send(`<a:WAVE:437630980480827403> **Ahsan#3247 has just left ${message.guild.name} :CRY: the server now has ${message.guild.memberCount} Members!** \nI guess Ahsan#3247 didnt want to follow the rules!`)
+    return message.channel.send(ahsanbanMsgemBed )}
+    
+    if (message.mentions.members.first().id === message.author.id){
+      return message.channel.send(`:x: ${message.author} You cannot do that to yourself, why did you try?`);
+    }
+    message.delete()
     banMember.ban(reason.join(" ")).then(member => {
      let serverBanmsg = new Discord.RichEmbed()
         .setTimestamp()
